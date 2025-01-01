01cb0980823090fd28ef533c978f853510eee14e
samilap <samicoding76@gmail.com>
2025-01-01 02:56:58 +0200

    Done task numpy02 (test ok)

--- assignments/02_numpy/src/my_code.py
+++ assignments/02_numpy/src/my_code.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+filein = 'm2in.npy'
+fileout = 'm2out.npy'
+
+taulukko = np.load(filein)
+
+taulukko[0,0] = 1
+
+last_row_index = taulukko.shape[0] - 1
+last_colum_index = taulukko.shape[1] - 1
+
+taulukko[0,0] = 1
+taulukko[last_row_index, last_colum_index] = -1
+
+np.save(fileout, taulukko)
